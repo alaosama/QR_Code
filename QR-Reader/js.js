49f3699116aa99563d702170e72e040b89f7c65a
alaosama <alahyaneosama1@gmail.com>
2024-03-26 11:57:53 +0100

    Add All Variables

---
+++ QR-Reader/js.js
@@ -0,0 +1,11 @@
+let QRImgUrl,is_img_selected = false,is_qr_code_decoded = false;
+
+let decoded_qr_text = document.querySelector("#decoded_qr_text");
+let qr_code_view_area = document-querySelector("•gr_code_view_area");
+let qr_code_img = document.querySelector("#qr_code_img");
+
+let input_select_img = document.querySelector("#input_select_img");
+let select_img_btn = document.querySelector("#select_img_btn");
+
+let read_qr_btn = document.querySelector("#read-qr-btn");
+let copy_qr_btn = document.queryselector("#copy-qr-btn");
