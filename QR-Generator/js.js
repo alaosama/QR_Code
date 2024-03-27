d2fb80a87b8398223fb89ad3789428a1f0fdac32
alaosama <alahyaneosama1@gmail.com>
2024-03-27 12:20:53 +0100

    QR Code Generator

--- QR-Generator/js.js
+++ QR-Generator/js.js
@@ -6,3 +6,20 @@ let qr_code_img = document.querySelector ("#qr_code_img");
 
 let generate_qr_btn = document.querySelector(".generate_qr_btn");
 let download_qr_btn = document.querySelector(".download_qr_btn");
+
+
+generate_qr_btn.addEventListener ("click",()=>{
+    if(is_qr_generated == false){
+        qr_txt = input_qr_text.value;
+    
+        if(qr_txt=="") {
+            alert ("Write some text!");
+        }else if(window.navigator.onLine!=true){
+            alert ("Oops! No internet connection");
+        }else {
+            GenerateQR();
+        }
+    }else {
+        
+    }
+});
