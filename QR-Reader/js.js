61ac9c09fc3bc344a5dc32a1e8f70c1953878a81
alaosama <alahyaneosama1@gmail.com>
2024-03-30 15:53:33 +0100

    QR Code Reader

--- QR-Reader/js.js
+++ QR-Reader/js.js
@@ -10,3 +10,32 @@ let select_img_btn = document.querySelector("#select_img_btn");
 let read_qr_btn = document.querySelector("#read-qr-btn");
 let copy_qr_btn = document.queryselector("#copy-qr-btn");
 
+input_select_img.addEventListener("change", function(){
+    const file = this.files[0];
+
+    if(file){
+        const reader = new FileReader();
+
+        select_img_btn.classList.add("hide_view");
+        qr_code_img.classList.remove("hide_view");
+
+        reader.addEventListener("load", function(){
+            QRImgUrl = this.result;
+            qr_code_img.setAttribute("src", this.result);
+        })
+
+        reader.readAsDataURL(file);
+        is_img_selected = true;
+    }
+})
+
+
+read_qr_btn.addEventListener("click", ()=> {
+    if(is_qr_code_decoded == false) {
+        if(is_img_selected == false) {
+            alert("Please select QR code image!");
+        } else {
+            ReadQRCode();
+        }
+    }
+})
