de816b47ddf936e13a57c4d715c65f4424c01416
Osama <144958643+alaosama@users.noreply.github.com>
2024-05-05 20:49:32 +0200

    QR Code Generator

--- QR-Generator_2/js.js
+++ QR-Generator_2/js.js
@@ -1,1 +1,17 @@
+let btn = document.querySelector(" button");
+let qr_code_element = document.querySelector("-qr-code");
+
+btn.addEventListener("click", () => {
+    let user_input = document.querySelector("#input_text");
+    if (user_input.value != "") {
+        if (qr_code_element.childElementCount == 0) {
+            generate(user_input);
+        } else {
+            qr_code_element.innerHTML = "";
+            generate(user_input);
+        }
+    } else {
+        qr_code_element.style = "display: none";
+    }
+});
 
