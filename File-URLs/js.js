5247b57f8f25f51d59d8ee3b84e609d0fb2305ac
alaosama <alahyaneosama1@gmail.com>
2024-06-03 14:56:21 +0200

    File URL

--- File-URLs/js.js
+++ File-URLs/js.js
@@ -1,1 +1,21 @@
-const input = document.querySelector('in'),
+const input = document.querySelector('input[type="file"]');
+
+input.addEventListener('change', () => {
+    
+    const file = input.files[0];
+
+    const f = new FileReader();
+
+    f.addEventListener('load', () => {
+        const url = f.result;
+
+        localStorage.setItem('file', url);
+
+        const img = new Image();
+        img.src = localStorage.getItem('file');
+
+        document.body.appendChild(img);
+
+        
+    })
+})
